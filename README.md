b55955c368ae90e7b192ace182d9522e0566813f
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-04-08 16:30:57 -0400

    updated cents in js file

--- README.md
+++ README.md
@@ -43,11 +43,11 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 	* [ ] `keydown`
 	* [x] `wheel`
 	* [ ] `drag / drop`
-	* [ ] `load`
-	* [ ] `focus`
+	* [x] `load`
+	* [x] `blur`
+	* [x] `focus`
 	* [x] `resize`
 	* [ ] `scroll`
-	* [ ] `select`
 	* [x] `dblclick`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
--- js/index.js
+++ js/index.js
@@ -28,7 +28,39 @@ document.addEventListener('wheel', () => {
 });
 
 
-// drag / drop event
+//load event -- add purple border to nav links
+const loadEvent = document.querySelector('img');
+loadEvent.addEventListener('load', () => {
+    return document.querySelector('.nav').style.border = '1px solid purple';
+});
 
 
+//focus and blur - change color of text input on form
+
+const focusNameInput = document.querySelector('#name');
+focusNameInput.addEventListener('focus', () => {
+    return focusNameInput.style.backgroundColor = "yellow"
+});
+
+const blurNameInput = document.querySelector('#name');
+blurNameInput.addEventListener('blur', () => {
+    return blurNameInput.style.backgroundColor = "skyblue"
+});
+
+const focusEmailInput = document.querySelector('#email');
+focusEmailInput.addEventListener('focus', () => {
+    return focusEmailInput.style.backgroundColor = "yellow"
+});
+
+const blurEmailInput = document.querySelector('#email');
+blurEmailInput.addEventListener('blur', () => {
+    return blurEmailInput.style.backgroundColor = "skyblue"
+});
+
+
+//scroll event - change font color on scroll
+const scrollColor = document.querySelector('p');
+document.addEventListener('scroll', () => {
+    return scrollColor.style.color = "blue";
+});
 
