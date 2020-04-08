ad68a6fb4cb33f5e2b11cdab80f6f6c7d3de5348
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-04-08 10:51:30 -0400

    updated the js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+
+//double click event -- change intro IMG
+const changeImg = document.querySelector('.intro img');
+changeImg.addEventListener('dblclick', () => {
+    changeImg.src = 'img/fun-bus2.jpg'
+});
+
+
+//resize event -- change bg color
+const changeBodyBg = document.querySelector('body');
+window.addEventListener('resize', () => {
+    return changeBodyBg.style.backgroundColor = 'springgreen';
+});
+
+
+//mouseover event -- give nav logo text a shadow
+const logoShadow = document.querySelector('.logo-heading');
+logoShadow.addEventListener('mouseover', () => {
+    return logoShadow.style.textShadow = '2px 2px 5px yellow';
+});
+
+
+//wheel event -- change color of bottom img
+const wheelImg = document.querySelector('.content-destination img');
+document.addEventListener('wheel', () => {
+    return wheelImg.style.filter = 'hue-rotate(65deg)';
+});
+
+
+// drag / drop event
+
+
+
